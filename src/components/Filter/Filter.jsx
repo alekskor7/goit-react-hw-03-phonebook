f4b204f4a724163c1209fd968a3b115f09923b84
Oleksandr Korniiets <alekskor7@gmail.com>
2021-11-14 18:54:27 +0200

    phonebook

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import Style from "./Filter.module.css";
+
+export default function Filter({ filter, onFilter }) {
+  return (
+    <form className={Style.form}>
+      <label>
+        <h3>Find contacts by name:</h3>
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={onFilter}
+          className={Style.input}
+        />
+      </label>
+    </form>
+  );
+}
+
+Filter.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
